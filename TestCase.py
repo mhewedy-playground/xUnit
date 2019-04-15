1eab968df4081dbb880b8ae4f0ff4efd01bf981b
Muhammad Hewedy <mhewedy@gmail.com>
2019-04-15 16:44:15 +0300

    strting ch 21

--- TestCase.py
+++ TestCase.py
@@ -2,25 +2,36 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def setup(self):
+        pass
+
     def run(self):
+        self.setup()
         getattr(self, self.name)()
+        self.tearDown()
+
+    def tearDown(self):
+        pass
 
 
 class WasRun(TestCase):
-    def __init__(self, name):
-        self.wasRun = None
-        TestCase.__init__(self, name)
+
+    def setup(self):
+        self.log = "setUp "
 
     def testMethod(self):
-        self.wasRun = 1
+        self.log = self.log + "testMethod "
+    
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
 
 class TestCaseTest(TestCase):
 
-    def testRunning(self):
+    def testTemplateMethod(self):
         test = WasRun("testMethod")
-        assert(not test.wasRun)
         test.run()
-        assert(test.wasRun)
+        assert(test.log == "setUp testMethod tearDown ")
 
-TestCaseTest("testRunning").run()
+
+TestCaseTest("testTemplateMethod").run()
